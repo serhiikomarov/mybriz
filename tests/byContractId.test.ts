25cd6ee7a684eb16f66c19a71a3062c917caf2c8
Sergey <sergey.komarov.atwork@gmail.com>
2025-02-10 23:10:47 +0200

    auth fixed

--- tests/byContractId.test.ts
+++ tests/byContractId.test.ts
@@ -2,13 +2,12 @@ import { test, expect } from '@playwright/test';
 import LoginPage from '../pages/authPage';
 import MainPage from '../pages/mainPage';
 
-test.describe('Auth by contract ID and password', () => {
+test.describe('Authorization by contract number and password', () => {
 
     let loginPage: LoginPage;
     let mainPage: MainPage;
 
     test.beforeEach(async ({ page }) => {
-        // Создаем объект для страницы логина
         loginPage = new LoginPage(page);
         mainPage = new MainPage(page);
 
