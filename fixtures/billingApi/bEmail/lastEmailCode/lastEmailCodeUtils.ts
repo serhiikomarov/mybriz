81129a22315a59aac9010291588a31854cc8824d
Sergey Komarov <sergey.komarov.atwork@gmail.com>
2025-02-15 11:06:58 +0200

    check confirmEmail completed

--- fixtures/billingApi/bEmail/lastEmailCode/lastEmailCodeUtils.ts
+++ fixtures/billingApi/bEmail/lastEmailCode/lastEmailCodeUtils.ts
@@ -2,7 +2,7 @@ import { expect } from "@playwright/test";
 import { getAuthToken } from "../../auth/authenticate/authUtils";
 
 export async function emailConfirmationCode(request: any, email: string) {
-  const apiUrl = `https://dev-bil-api.briz.ua/email/lastcode`;
+  const apiUrl = "https://dev-bil-api.briz.ua/email/lastcode";
   const token = await getAuthToken(request);
   const response = await request.get(apiUrl, {
     headers: {
