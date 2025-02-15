f7ba00c279b393469bd45ebc727343feef8422da
Sergey Komarov <sergey.komarov.atwork@gmail.com>
2025-02-15 15:39:43 +0200

    Authorization by email and password use correct data completed

--- tests/byEmail.test.ts
+++ tests/byEmail.test.ts
@@ -5,16 +5,30 @@ import { globalData } from "../fixtures/global.data";
 import { testData } from "../testData/test.data";
 import { errorMessages } from "../testData/errors.data";
 import { createBUser } from "../fixtures/billingApi/bUser/bUserCreate/bUserCreateUtils";
-import { updatePassword } from "../fixtures/billingApi/bUser/updatePassword/updatePasswordUtils";
+import { addEmail } from "../fixtures/billingApi/bUser/setEmail/setEmailUtils";
+import { emailConfirmationCode } from "../fixtures/billingApi/bEmail/lastEmailCode/lastEmailCodeUtils";
+import { confirmEmail } from "../fixtures/billingApi/bEmail/confirmEmail/confirmEmailUtils";
+import { generateRandomEmail } from "../fixtures/helpers";
 
 test.describe("Authorization by email and password", () => {
   let loginPage: LoginPage;
   let mainPage: MainPage;
   let userID: string;
+  let email: string;
+  let code: string;
 
   test.beforeAll(async ({ browser }) => {
     const context = await browser.newContext();
+    // create user and get contract ID
     userID = String(await createBUser(context.request));
+    // generate email
+    email = generateRandomEmail();
+    // add email to user
+    await addEmail(context.request, userID, email);
+    // get confirmation code
+    code = await emailConfirmationCode(context.request, email);
+    // confirm email
+    await confirmEmail(context.request, code);
   });
 
   test.beforeEach(async ({ page }) => {
@@ -23,8 +37,20 @@ test.describe("Authorization by email and password", () => {
     await loginPage.navigateToLoginPage();
   });
 
-  test("Authorization by contract number and password with minimum length password", async ({ page }) => {
-    await loginPage.login(userID, globalData.defaultPassword);
+  test("Authorization by email and password correct data", async ({ page }) => {
+    await loginPage.login(email, globalData.defaultPassword);
     await page.waitForURL(mainPage.pageUrl);
   });
 });
+
+// Заполнить только поле Email, а поле Password оставить пустым.
+
+// Тестирование некорректных данных / например, test@com или test.com).
+
+// Ввести неверный Email, которые не существуют в базе).
+
+// Тестирование формата и символов / Ввести в поле Email специальные символы или пробелы
+
+// Ввести в поле Password специальные символы, чтобы проверить обработку (например, @#12abcd).
+
+//SQL-инъекцию или XSS-атаки в поля Email и Password (например, <script>alert('XSS')</script>).
