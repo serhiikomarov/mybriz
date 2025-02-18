cae49c7c2f1877d84b8eae2c2a5e518079dbf3ec
Sergey Komarov <sergey.komarov.atwork@gmail.com>
2025-02-18 18:52:45 +0200

    addMoney fixture added

---
+++ fixtures/billingApi/addMoney/addMoneyUtils.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+import { createBUser } from "../bUserCreate/bUserCreateUtils";
+import { addMoney } from "./addMoneyUtils";
+import { globalData } from "../../../global.data";
+
+test.describe("Checking update user password function", () => {
+  let userID: string;
+
+  test.beforeAll(async ({ browser }) => {
+    const context = await browser.newContext();
+    const createdUser = await createBUser(context.request);
+    userID = String(createdUser);
+  });
+
+  test("Checking update user password function", async ({ browser }) => {
+    const context = await browser.newContext();
+    const response = await addMoney(context.request, userID, { amount: 100 });
+    console.log(userID);
+  });
+});
---
+++ fixtures/billingApi/addMoney/addMoneyUtils.ts
@@ -0,0 +1,19 @@
+import { expect } from "@playwright/test";
+import { getAuthToken } from "../../auth/authenticate/authUtils";
+
+export async function addMoney(request: any, userID: string, data: any) {
+  const apiUrl = `https://dev-bil-api.briz.ua/buser/${userID}/money/direct`;
+  const token = await getAuthToken(request);
+  const response = await request.post(apiUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    /* 
+    Data examples: 
+    {"amount": 100} - 100 UAH will be credited to the balance.
+    {"bonus": 100} - 100 bonuses will be credited to the balance.
+    */
+    data: data,
+  });
+}
