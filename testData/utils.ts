b4dc04c0484efd34569812e7a01154e3aaa824c1
Sergey Komarov <sergey.komarov.atwork@gmail.com>
2025-03-03 10:16:09 +0200

    function toPayAmountFunc updated

--- testData/utils.ts
+++ testData/utils.ts
@@ -1,6 +1,6 @@
 import { daysInMonth, remainingDaysInMonth } from '../fixtures';
 
-async function toPayAmountFunc(months: number, servicePrice: any) {
+function toPayAmountFunc(months: number, servicePrice: any) {
 	if (months === 1) return (servicePrice / daysInMonth()) * remainingDaysInMonth();
 	return servicePrice * months;
 }
