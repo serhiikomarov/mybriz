ca0fd091c6da000516a2cd0b81f2edd1e1c27553
Sergey Komarov <sergey.komarov.atwork@gmail.com>
2025-03-10 22:45:45 +0200

    authWithSMSCodePage POM added

--- pages/index.ts
+++ pages/index.ts
@@ -2,3 +2,5 @@ export * from "./authPage";
 export * from "./mainPage";
 export * from "./paymentPage";
 export * from "./thankYouPage";
+export * from "./authWithSMSPage";
+export * from "./authWithSMSCodePage";
