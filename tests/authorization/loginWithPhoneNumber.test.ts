b8e9d8aab2422eaf80100cd811001cf4267daa29
Sergey Komarov <sergey.komarov.atwork@gmail.com>
2025-03-26 10:15:02 +0200

    Authorization by login with empty phone number field completed plus updates

--- tests/authorization/loginWithPhoneNumber.test.ts
+++ tests/authorization/loginWithPhoneNumber.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage, MainPage, AuthTwoFactorPage } from '../../pages';
-import { createBUser, generatePhoneNumber, addPhoneNumber, createInternetAccount, getInternetAccountID, generateLogin, changeLanguage, getPhoneMask, waitForSMSCode } from '../../fixtures';
+import { createBUser, generatePhoneNumber, addPhoneNumber, getInternetAccountID, generateLogin, changeLanguage, getPhoneMask, waitForSMSCode } from '../../fixtures';
 import { setLanguage, errorMessages, globalData } from '../../testData';
 
 test.describe('Authorization by login with phone number', () => {
@@ -8,28 +8,36 @@ test.describe('Authorization by login with phone number', () => {
 	let mainPage: MainPage;
 	let authTwoFactorPage: AuthTwoFactorPage;
 	let userID: number;
-	let internetAccountID: number;
+	let userID2: number;
+	let userID3: number;
 	let internetLogin: string;
+	let internetLogin2: string;
+	let internetLogin3: string;
 	let phoneNumber: string;
+	let phoneNumber2: string;
 	let codeFromSMS: string;
 
 	test.beforeAll(async ({ browser }) => {
 		const context = await browser.newContext();
 
-		// Create user and get contract ID
+		// --- Сreating the first user with a phone number ---
 		userID = await createBUser(context.request);
-
-		// Generate Internet login
 		internetLogin = generateLogin(10);
-
-		// Add Internet Account ot user
-		internetAccountID = await getInternetAccountID(context.request, userID, internetLogin);
-
-		// Generate phone number
+		await getInternetAccountID(context.request, userID, internetLogin);
 		phoneNumber = generatePhoneNumber();
-
-		// Add phone number to user
 		await addPhoneNumber(context.request, userID, phoneNumber);
+
+		// --- Сreating a second user with a phone number ---
+		userID2 = await createBUser(context.request);
+		internetLogin2 = generateLogin(10);
+		await getInternetAccountID(context.request, userID2, internetLogin2);
+		phoneNumber2 = generatePhoneNumber();
+		await addPhoneNumber(context.request, userID2, phoneNumber2);
+
+		// --- Creating a third user without a phone number ---
+		userID3 = await createBUser(context.request);
+		internetLogin3 = generateLogin(10);
+		await getInternetAccountID(context.request, userID3, internetLogin3);
 	});
 
 	test.beforeEach(async ({ page }) => {
@@ -96,41 +104,21 @@ test.describe('Authorization by login with phone number', () => {
 		await page.waitForURL(mainPage.pageUrl);
 	});
 
-	// test('Authorization by login with phone number and correct data', async ({ page, request }) => {
-	// 	await loginPage.login(internetLogin, globalData.defaultInternetPassword);
-	// 	await page.waitForURL(authTwoFactorPage.pageUrl);
-	// 	await expect(authTwoFactorPage.backButton).toHaveText(authTwoFactorPage.texts.ua.backButton);
-	// 	await expect(authTwoFactorPage.title).toHaveText(authTwoFactorPage.texts.ua.title);
-	// 	await expect(authTwoFactorPage.discription).toHaveText(authTwoFactorPage.texts.ua.phoneNumberDiscription);
-	// 	await expect(authTwoFactorPage.phoneFormText).toHaveText(authTwoFactorPage.texts.ua.phoneFormText);
-	// 	const phoneMask = getPhoneMask(phoneNumber);
-	// 	await expect(authTwoFactorPage.maskedPhoneNumber).toHaveText(phoneMask);
-	// 	await expect(authTwoFactorPage.changePhoneNumberButton).toHaveText(authTwoFactorPage.texts.ua.changePhoneNumberButton);
-	// 	await expect(authTwoFactorPage.sendCodeButton).toHaveText(authTwoFactorPage.texts.ua.sendCodeButtonText);
-	// 	await expect(authTwoFactorPage.socialsText).toHaveText(authTwoFactorPage.texts.ua.socialsText);
-	// 	expect(authTwoFactorPage.appleIDAuthButton).toBeVisible();
-	// 	expect(authTwoFactorPage.googleAuthButton).toBeVisible();
-	// 	expect(authTwoFactorPage.facebookAuthButton).toBeVisible();
-	// 	authTwoFactorPage.sendCodeButton.click();
-	// 	await expect(authTwoFactorPage.backButton).toHaveText(authTwoFactorPage.texts.ua.backButton);
-	// 	await expect(authTwoFactorPage.title).toHaveText(authTwoFactorPage.texts.ua.title);
-	// 	await expect(authTwoFactorPage.discription).toHaveText(authTwoFactorPage.texts.ua.codeFromSMSDiscription);
-	// 	await expect(authTwoFactorPage.logInButton).toHaveText(authTwoFactorPage.texts.ua.loginButtonText);
-	// 	await expect(authTwoFactorPage.socialsText).toHaveText(authTwoFactorPage.texts.ua.socialsText);
-	// 	expect(authTwoFactorPage.appleIDAuthButton).toBeVisible();
-	// 	expect(authTwoFactorPage.googleAuthButton).toBeVisible();
-	// 	expect(authTwoFactorPage.facebookAuthButton).toBeVisible();
-	// 	codeFromSMS = await waitForSMSCode(request, phoneNumber);
-	// 	await authTwoFactorPage.enterCodeFromSMS(codeFromSMS);
-	// 	await expect(authTwoFactorPage.codeInputFourthDigit).toHaveValue(Array.from(codeFromSMS)[3]);
-	// 	authTwoFactorPage.logInButton.click();
-	// 	await page.waitForURL(mainPage.pageUrl);
-	// });
+	test('Authorization by login with empty phone number field', async ({ page, request }) => {
+		await loginPage.login(internetLogin3, globalData.defaultInternetPassword);
+		await page.waitForURL(authTwoFactorPage.pageUrl);
+		await authTwoFactorPage.sendCodeButton.click();
+		expect(authTwoFactorPage.inputHelper).toHaveText(errorMessages.ua.fieldRequired);
+		// Checking input error in English localization
+		await changeLanguage(page, setLanguage.en);
+		await authTwoFactorPage.sendCodeButton.click();
+		expect(authTwoFactorPage.inputHelper).toHaveText(errorMessages.en.fieldRequired);
+	});
 
 	/*
 	incorrect login and password
-	empty phone number field
 	phone number from another account
+	success change phone number flow
 	invalid phone number
 	empty code field
 	incorrect code
