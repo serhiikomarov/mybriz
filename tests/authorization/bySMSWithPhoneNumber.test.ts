77e4d05f8cad4022276d4b14c89cb1b2420dbb62
Sergey Komarov <sergey.komarov.atwork@gmail.com>
2025-04-07 21:59:52 +0300

    auth tests have been updated

--- tests/authorization/bySMSWithPhoneNumber.test.ts
+++ tests/authorization/bySMSWithPhoneNumber.test.ts
@@ -1,74 +1,74 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage, MainPage, AuthWithSMSPage, AuthWithSMSCodePage } from '../../pages';
-import { createBUser, generatePhoneNumber, addPhoneNumber, waitForSMSCode, changeLanguage } from '../../fixtures';
-import { setLanguage, errorMessages, globalData } from '../../testData';
+import { test, expect } from "@playwright/test";
+import { LoginPage, MainPage, AuthWithSMSPage, AuthWithSMSCodePage } from "../../pages";
+import { createBUser, generatePhoneNumber, addPhoneNumber, waitForSMSCode, changeLanguage } from "../../fixtures";
+import { setLanguage, errorMessages, globalData } from "../../testData";
 
-test.describe('Authorization by SMS with phone number', () => {
-	let loginPage: LoginPage;
-	let mainPage: MainPage;
-	let authWithSMSPage: AuthWithSMSPage;
-	let authWithSMSCodePage: AuthWithSMSCodePage;
-	let userID: number;
-	let phoneNumber: string;
-	let codeFromSMS: string;
+test.describe("Authorization by SMS with phone number", () => {
+  let loginPage: LoginPage;
+  let mainPage: MainPage;
+  let authWithSMSPage: AuthWithSMSPage;
+  let authWithSMSCodePage: AuthWithSMSCodePage;
+  let userID: number;
+  let phoneNumber: string;
+  let codeFromSMS: string;
 
-	test.beforeAll(async ({ browser }) => {
-		const context = await browser.newContext();
+  test.beforeAll(async ({ browser }) => {
+    const context = await browser.newContext();
 
-		// Create user and get contract ID
-		userID = await createBUser(context.request);
+    // Create user and get contract ID
+    userID = await createBUser(context.request);
 
-		// Generate phone number
-		phoneNumber = generatePhoneNumber();
+    // Generate phone number
+    phoneNumber = generatePhoneNumber();
 
-		// Add phone number to user
-		await addPhoneNumber(context.request, userID, phoneNumber);
-	});
+    // Add phone number to user
+    await addPhoneNumber(context.request, userID, phoneNumber);
+  });
 
-	test.beforeEach(async ({ page }) => {
-		loginPage = new LoginPage(page);
-		mainPage = new MainPage(page);
-		authWithSMSPage = new AuthWithSMSPage(page);
-		authWithSMSCodePage = new AuthWithSMSCodePage(page);
-		await loginPage.navigateToLoginPage();
-		await loginPage.authWithSMSButton.click();
-	});
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    mainPage = new MainPage(page);
+    authWithSMSPage = new AuthWithSMSPage(page);
+    authWithSMSCodePage = new AuthWithSMSCodePage(page);
+    await loginPage.navigateToLoginPage();
+    await loginPage.authWithSMSButton.click();
+  });
 
-	test('Authorization by SMS with correct phone number and correct code from SMS', async ({ page, request }) => {
-		await authWithSMSPage.sendSMS(phoneNumber);
-		codeFromSMS = await waitForSMSCode(request, phoneNumber);
-		authWithSMSCodePage.enterCodeFromSMS(codeFromSMS);
-		// checking for last element of input has been filled
-		await expect(authWithSMSCodePage.codeInputFourthDigit).toHaveValue(Array.from(codeFromSMS)[3]);
-		await authWithSMSCodePage.logInButton.click();
-		await page.waitForURL(mainPage.pageUrl);
-	});
+  test("Authorization by SMS with correct phone number and correct code from SMS", async ({ page, request }) => {
+    await authWithSMSPage.sendSMS(phoneNumber);
+    codeFromSMS = await waitForSMSCode(request, phoneNumber);
+    authWithSMSCodePage.enterCodeFromSMS(codeFromSMS);
+    // checking for last element of input has been filled
+    await expect(authWithSMSCodePage.codeInputFourthDigit).toHaveValue(Array.from(codeFromSMS)[3]);
+    await authWithSMSCodePage.logInButton.click();
+    await page.waitForURL(mainPage.pageUrl);
+  });
 
-	test('Authorization by SMS with phone number not in database UA', async () => {
-		await authWithSMSPage.sendSMS(globalData.phoneNumberNotInDatabase);
-		await expect(authWithSMSPage.inputHelper).toContainText(errorMessages.ua.incorrectAccountNumberCode);
-	});
+  test("Authorization by SMS with phone number not in database UA", async () => {
+    await authWithSMSPage.sendSMS(globalData.phoneNumberNotInDatabase);
+    await expect(authWithSMSPage.inputHelper).toContainText(errorMessages.ua.incorrectAccountNumberCode);
+  });
 
-	test('Authorization by SMS with phone number not in database EN', async ({ page }) => {
-		await changeLanguage(page, setLanguage.en);
-		await authWithSMSPage.sendSMS(globalData.phoneNumberNotInDatabase);
-		await expect(authWithSMSPage.inputHelper).toContainText(errorMessages.en.incorrectAccountNumberCode);
-	});
+  test("Authorization by SMS with phone number not in database EN", async ({ page }) => {
+    await changeLanguage(page, setLanguage.en);
+    await authWithSMSPage.sendSMS(globalData.phoneNumberNotInDatabase);
+    await expect(authWithSMSPage.inputHelper).toContainText(errorMessages.en.incorrectAccountNumberCode);
+  });
 
-	test('Authorization by SMS with ivalid phone number UA', async () => {
-		await authWithSMSPage.sendSMS(globalData.invalidPhoneNumber);
-		await expect(authWithSMSPage.inputHelper).toContainText(errorMessages.ua.invalidPhoneNumber);
-	});
+  test("Authorization by SMS with invalid phone number UA", async () => {
+    await authWithSMSPage.sendSMS(globalData.incorrectFormatPhoneNumber);
+    await expect(authWithSMSPage.inputHelper).toContainText(errorMessages.ua.invalidPhoneNumber);
+  });
 
-	test('Authorization by SMS with ivalid phone number EN', async ({ page }) => {
-		await changeLanguage(page, setLanguage.en);
-		await authWithSMSPage.sendSMS(globalData.invalidPhoneNumber);
-		await expect(authWithSMSPage.inputHelper).toContainText(errorMessages.en.invalidPhoneNumber);
-	});
+  test("Authorization by SMS with invalid phone number EN", async ({ page }) => {
+    await changeLanguage(page, setLanguage.en);
+    await authWithSMSPage.sendSMS(globalData.incorrectFormatPhoneNumber);
+    await expect(authWithSMSPage.inputHelper).toContainText(errorMessages.en.invalidPhoneNumber);
+  });
 
-	test('Authorization by SMS checking more than 10 digits in input', async ({ page }) => {
-		await authWithSMSPage.phoneNumberInput.pressSequentially('09876543210');
-		const value = await authWithSMSPage.phoneNumberInput.inputValue();
-		expect(value).toBe('0987654321');
-	});
+  test("Authorization by SMS checking more than 10 digits in input", async ({ page }) => {
+    await authWithSMSPage.phoneNumberInput.pressSequentially("09876543210");
+    const value = await authWithSMSPage.phoneNumberInput.inputValue();
+    expect(value).toBe("0987654321");
+  });
 });
