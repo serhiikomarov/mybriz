e25686e4c1e45d6ca34c956b0ddbf8b59e5a1cd0
Sergey Komarov <sergey.komarov.atwork@gmail.com>
2025-10-22 17:50:19 +0300

    user 2 created + precondition updated

--- tests/authorization/multiAccountPrecondition.test.ts
+++ tests/authorization/multiAccountPrecondition.test.ts
@@ -9,18 +9,55 @@ test.describe('kjsdkljfksldjflks sfd sd', () => {
 	let userID1: number;
 	let userID2: number;
 	let userID3: number;
+	let userID4: number;
+	let userID5: number;
+	let userID6: number;
+	let userID7: number;
 	let internetLogin1: string;
+	let internetLogin2: string;
+	let internetLogin3: string;
+	let internetLogin4: string;
+	let internetLogin5: string;
+	let internetLogin6: string;
+	let internetLogin7: string;
 
 	test.beforeAll(async ({ browser }) => {
 		const context = await browser.newContext();
 
-		// Create Account #1 - Administrator
+		// Create Account #1 - Администратор
 		userID1 = await createBUser(context.request);
 		internetLogin1 = generateLogin(10);
-		console.log(userID1);
-		const responseBody = await createInternetAccount(context.request, userID1, internetLogin1);
-		let accountID = responseBody.data;
-		return accountID;
+		console.log('\x1b[33m%s\x1b[0m', `Аккаунт 1 http://dbilling.briz.ua/Ru/billing/user/view.html?uid=${userID1}`);
+
+		// Create Account #2
+		userID2 = await createBUser(context.request);
+		internetLogin2 = generateLogin(10);
+		console.log('\x1b[33m%s\x1b[0m', `Аккаунт 2 http://dbilling.briz.ua/Ru/billing/user/view.html?uid=${userID2}`);
+
+		/* Create Account #3
+		userID3 = await createBUser(context.request);
+		internetLogin3 = generateLogin(10);
+		console.log(`Аккаунт 3 ${userID3}`);
+
+		// Create Account #4
+		userID4 = await createBUser(context.request);
+		internetLogin4 = generateLogin(10);
+		console.log(`Аккаунт 4 ${userID4}`);
+
+		// Create Account #5
+		userID5 = await createBUser(context.request);
+		internetLogin5 = generateLogin(10);
+		console.log(`Аккаунт 5 ${userID5}`);
+
+		// Create Account #6
+		userID6 = await createBUser(context.request);
+		internetLogin6 = generateLogin(10);
+		console.log(`Аккаунт 6 ${userID6}`);
+
+		// Create Account #7
+		userID7 = await createBUser(context.request);
+		internetLogin7 = generateLogin(10);
+		console.log(`Аккаунт 7 ${userID7}`); */
 	});
 
 	test.beforeEach(async ({ page }) => {
